6e21991cf6ba0eec3babfa2c8aec1e3ef3da0b7b
Anushka Rastogi <97504525+anushkarastogi@users.noreply.github.com>
2022-10-06 00:24:59 +0530

    Initial commit

---
+++ src/MainContent.js
@@ -0,0 +1,14 @@
+import React from "react"
+export default function MainContent(){
+  return(
+    <div>
+      <h1>
+        Reasons I'm excited to learn React
+      </h1>
+      <ol>
+        <li>It's popular library, so I'll be able to fit in with the kids</li>
+        <li>I'm more likely to get job as a Developer if I learn React</li>
+      </ol>
+    </div>
+  )
+}
